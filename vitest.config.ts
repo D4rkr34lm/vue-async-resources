605f711c8e8caebb8d66d033d8be0ce1499aa487
Manuel Frohn <manuel.frohn.09.12@gmail.com>
2025-11-12 13:17:56 +0100

    Add config for testing

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from "url";
+import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
+
+export default 
+  defineConfig({
+    test: {
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude],
+      root: fileURLToPath(new URL("./", import.meta.url)),
+      coverage: {
+        provider: "v8",
+        reporter: ["html-spa", "text"],
+        include: ["*"],
+        reportsDirectory: "./coverage",
+        enabled: true,
+        cleanOnRerun: true,
+      },
+    },
+  })
