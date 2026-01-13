ca61d5af65406ebff34c9f40ecf8b93dde3acba3
D4rkr34lm <darth.m.frohn@gmail.com>
2026-01-13 20:58:09 +0100

    Add barell file

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { AsyncResources } from "./plugin.js";
+
+export { defineResource } from "./resource/defineResource.js";
+export { defineResourceAction } from "./action/defineResourceAction.js";
+export { defineResourceCollection } from "./collection/defineResourceCollection.js";
+export { useCombinedResources } from "./linked/useLinkedResource.js";
+
+export default AsyncResources;
